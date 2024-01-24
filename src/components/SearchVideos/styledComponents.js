c7cdd5bbbc8488c460fa67d42dc2910af855b9dc
abhishekchandra <shek.7.abhishek@gmail.com>
2024-01-24 08:03:27 +0000

    Assignment-7 | Nxt Watch

---
+++ src/components/SearchVideos/styledComponents.js
@@ -0,0 +1,86 @@
+import {Link} from 'react-router-dom'
+import styled from 'styled-components'
+
+export const SearchVideosContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${props => props.bgColor};
+  overflow-y: scroll;
+  padding: 30px;
+`
+
+export const SearchInput = styled.input`
+  width: 250px;
+  border: 1px solid #64748b;
+  border-radius: 2px;
+  margin-left: 60px;
+  padding: 3px;
+  padding-right: 10px;
+  padding-left: 10px;
+`
+
+export const VideosContainer = styled.ul`
+  display: flex;
+  flex-direction: row;
+  list-style-type: none;
+  flex-wrap: wrap;
+`
+
+export const ProductsLoaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+
+  @media screen and (min-width: 768px) {
+    width: 70%;
+  }
+`
+
+export const NotFoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-color: #ffffff;
+`
+
+export const Image = styled.img`
+  width: 300px;
+  margin: 20px;
+  align-self: center;
+`
+
+export const Heading = styled.h1`
+  color: black;
+  text-align: center;
+  font-size: 30px;
+`
+
+export const Desc = styled.p`
+  color: black;
+  text-align: center;
+  font-size: 20px;
+`
+
+export const NavLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #1e293b;
+  text-decoration: none;
+  margin-bottom: 32px;
+`
+
+export const Retry = styled.button`
+  padding: 15px;
+  background-color: #4f46e5;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 35px 10px 35px;
+  cursor: pointer;
+  color: #ffffff;
+  align-self: center;
+`
